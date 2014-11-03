73f21ed332b2baeb1dbb68dd997c5fae30f1136f
Arjun Mathur <arjunm@stanford.edu>
2014-11-03 13:52:48 -0800

    Updated simrank.py. Added k_hops forulation and decreased space complexity

--- simrank.py
+++ simrank.py
@@ -1,4 +1,6 @@
 import networkx as nx
+import collections
+import itertools
 #from networkx.algorithms import bipartite	
 										
 def initialize():
@@ -6,67 +8,78 @@ def initialize():
 	# 'Uf61AA2JUGWTcxSXNoGaXg', 'xGz3WSkC96aWtqt7vE70gw'
 	return G
 
-def get_nodes_list(user,business):
-	with open("user_id.txt") as file:
-		for line in file:
-			user.append(line)
-	with open("business_id.txt") as file:
-		for line in file:
-			business.append(line)
+def sim_initialize():
+	user_sim, business_sim = {}, {}
+	
+	with open("user_id.txt") as f:
+		for line in f:
+			node = line.rstrip('\n')
+			user_sim[node] = collections.Counter()
+			user_sim[node][node] = 1.0
+		
+	with open("business_id.txt") as f:
+		for line in f:
+			node = line.rstrip('\n')
+			business_sim[node] = collections.Counter()
+			business_sim[node][node] = 1.0
 
-def sim_initialize(G,user,business,user_sim,business_sim):
-	for node1 in user:
-		user_sim[node1] = {}
-		for node2 in user:
-			if node1 == node2:
-				user_sim[node1][node2] = 1
-			else:
-				user_sim[node1][node2] = 0
-	for node1 in business:
-		business_sim[node1] = {}
-		for node2 in business:
-			if node1 == node2:
-				business_sim[node1][node2] = 1
-			else:
-				business_sim[node1][node2] = 0	
+	return user_sim, business_sim
+
+def k_hops(G, node, k):
+	'''Calculates all nodes within k edges from @node
+		 that are on the same part of a bipartite graph'''
+	result = set([])
+	seen = set([node])
+	search = set([node])
+	for i in range(k):
+		search = set([nbr for n in search for nbr in G.neighbors(n) if nbr not in seen])
+		seen |= search
+		if i%2 == 1: result |= search # Ensure we get only one part of the bipartite graph
+
+	return result
 
 
-def calculate_sim(user,business,user_sim,business_sim):
-	for node1 in user:
-		out1 = [x for x in G[node1]]
-		weight1 = [G[node1][x]['weight'] for x in G[node1]]
-		for node2 in user:
-			out2 = [x for x in G[node2]]
-			weight2 = [G[node2][x]['weight'] for x in G[node2]]
-			num = sum([G[node1][y]['weight']*sum([G[node2][x]['weight']*business_sim[x][y] for x in out2]) for y in out1])	
-			den = sum(weight1)*sum(weight2)
-			user_sim[node1][node2] = C1*num/den
+def calculate_sim(G, user_sim, business_sim):
+	def calculate(from_sim, to_sim, C):
+		''' Calculates similarity of items in from_sim using similarity of items in to_sim.
+				Performs one part of the bipartite simrank algorithm'''
+		# For every pair of nodes
+		for node1, node2 in itertools.combinations(from_sim, 2):
+			num = 0
+			for a in G.neighbors_iter(node1):
+				#for b in k_hops(G, a, 5):
+				for b in G.neighbors_iter(node2):
+					x, y = sorted((a, b)) #Keep only the upper half of the matrix
+					try:num += G[node1][a]['weight'] * G[node2][b]['weight'] * to_sim[x][y]
+					except Exception, e: import pdb;pdb.set_trace()
 
-	for node1 in business:
-		out1 = [x for x in G[node1]]
-		weight1 = [G[node1][x]['weight'] for x in G[node1]]
-		for node2 in business:
-			out2 = [x for x in G[node2]]
-			weight2 = [G[node2][x]['weight'] for x in G[node2]]
-			num = sum([G[node1][y]['weight']*sum([G[node2][x]['weight']*user_sim[x][y] for x in out2]) for y in out1])	
-			den = sum(weight1)*sum(weight2)
-			business_sim[node1][node2] = C2*num/den
-			
+			weight1 = sum(G[node1][x]['weight'] for x in G.neighbors_iter(node1))
+			weight2 = sum(G[node2][x]['weight'] for x in G.neighbors_iter(node2))
+			den = weight1*weight2
+
+			from_sim[node1][node2] = C*num/den # Note: itertools.combinations emits in sorted order
+
+	calculate(user_sim, business_sim, C1)
+	calculate(business_sim, user_sim, C2)
+
+########### Constants ############			
 C1 = 0.8
 C2 = 0.8
-G = initialize()
-print "initialized"
-user = []
-business = []
-user_sim = {}
-business_sim = {}
+USER_FILE = 'user_id.txt'
+BUSINESS_FILE = 'business_id.txt'
+N_ITERS = 10
+##################################
 
-get_nodes_list(user,business)
-print "get_node_lists done"
+def main():
+	G = initialize()
+	print "Initialized"
 
-sim_initialize(G,user,business,user_sim,business_sim)
-print "sim initialized"
+	user_sim, business_sim = sim_initialize()
+	print "sim initialized"
 
-for i in range(10):
-	calculate_sim(user,business,user_sim,business_sim)
+	for _ in range(N_ITERS):
+		print _
+		calculate_sim(G, user_sim, business_sim)
 
+if __name__ == '__main__':
+	main()
