e5bc37459bd04858d49f5f564f47548973be84f6
Shubham Gupta <shubham.gupta3112@gmail.com>
2014-11-10 02:42:19 -0800

    To-do

--- analysis.py
+++ analysis.py
@@ -3,6 +3,8 @@ import collections
 import itertools
 import matplotlib.pyplot as plt
 import json
+import numpy as np
+
 
 def initialize():
 	G = nx.read_edgelist("edges.txt", nodetype=str, data=(('weight',float),))
@@ -31,15 +33,57 @@ def k_hops(G, node, k):
 		seen |= search
 		if i%2 == 1: result |= search # Ensure we get only one part of the bipartite graph
 
-	return result
+	return len(result)
 
+def till_k_hops(G, node, k):
+	'''Calculates all nodes within k edges from @node
+		 that are on the same part of a bipartite graph'''
+	hop = collections.Counter()	 
+	seen = set([node])
+	search = set([node])
+	for i in range(k):
+		#search = set([nbr for n in search for nbr in G[n]])
+		search = set([nbr for n in search for nbr in G[n] if nbr not in seen])
+		seen |= search
+		if i%2 == 1: 
+			hop[i+1] = hop[i-1] + len(search)
+	return hop
+
+def alphaMLE(data,xmin):
+	L = 0
+	for i in data:
+		L += np.log(i*1.0/xmin)
+	alpha_hat = 1 + len(data)*1.0/L
+	return alpha_hat	
+
+def degree_distribution(G, nodes):
+	nodes = list(set(nodes)&set(G.nodes()))
+	degree_sequence=[G.degree(x) for x in nodes]
+	dist = collections.Counter(degree_sequence)
+	return dist, degree_sequence
+
+def logplot(x,y):
+	plt.loglog(x,y)
+	plt.show()	
+
+def hop_distribution(G,nodes,k):
+	hop = {}
+	for node in (set(nodes) & set(G.nodes())):
+		hop[node] = k_hops(G,node,k)
+
+	total_pair = sum(hop.values())*0.5
+	hop_dist = collections.Counter(hop.values())
+	return hop_dist, total_pair
+
+def k_hop_nodewise(G,nodes,k):
+	hop_count = {}
+	for node in (set(nodes) & set(G.nodes())):
+		hope_count[node] = till_k_hops(G,node,k)
+	return hop_count
 
 ########### Constants ############			
-C1 = 0.8
-C2 = 0.8
 USER_FILE = 'user_id.txt'
 BUSINESS_FILE = 'business_id.txt'
-N_ITERS = 10
 ##################################
 
 def main():
@@ -48,42 +92,47 @@ def main():
 	print "initialized"
 	user, business = node_initialize(G)
 	print "sim initialized"	
-	G1 = nx.cycle_graph(10)
-	#print user[1]
-	#print type(G.nodes()[1])
+	'''
+	user_degree_dist, user_degree_sequence = degree_distribution(G,user)
+	logplot(user_degree_sequence.keys(), user_degree_dist.values())
+
+	business_degree_dist, business_degree_sequence = degree_distribution(G,business)
+	logplot(business_degree_sequence.keys(), business_degree_dist.values())
 	
-	degree_sequence=[G.degree(x) for x in user]
-	dist = collections.Counter(degree_sequence)
-	plt.loglog(dist.values())
+	alpha_user = alphaMLE(user_degree_sequence, 1)
+	business_degree_sequence = [x for x in business_degree_sequence if x > 1]
+	alpha_business = alphaMLE(business_degree_sequence,2)
+	print alpha_user, alpha_business
+	'''
+	user_hop_dist, total_user_pair = hop_distribution(G,user,2)
+	business_hop_dist, total_business_pair = hop_distribution(G,business,2)
+	#print business_hop_dist
+	print 2.0*total_user_pair/len(set(user) & set(G.nodes()))
+	print 2.0*total_business_pair/len(set(business) & set(G.nodes()))
+	#print total_business_pair
+	## 2-hop Values : 135217058.0, 17896957.0
+	## Average at least 1 common rated user =  1069.3406669
+	## ## Average at least 1 common rater business =  853.089136756
+	
+	plt.plot(user_hop_dist.keys(),user_hop_dist.values())
+	plt.xscale('log')
+	plt.show()
+	plt.plot(business_hop_dist.keys(), business_hop_dist.values())
+	plt.xscale('log')
 	plt.show()
 	
-	degree_sequence1=[G.degree(x) for x in business]
-	
-	dist1 = {}
-	for n in degree_sequence1:
-		if n in dist1.keys():
-			dist1[n] += 1
-		else:
-			if type(n) is int:	
-				dist1[n] = 1		
 
-	plt.loglog(dist1.values())
+	'''
+	user_hop_count = k_hop_nodewise(G,user,8)
+	business_hop_count = k_hop_nodewise(G,business,8)
+
+	plt.plot(user_hop_dist.values())
+	plt.xscale('log')
 	plt.show()
-	
+	plt.plot(business_hop_dist.values())
+	plt.xscale('log')
+	plt.show()
 	'''
-	hop = {}
-	#for node in (set(business) & set(G.nodes())):
-	for node in G1.nodes():	
-		#print node
-		hop[node] = len(k_hops(G1,node,2))
-		print node, hop[node]
-	
-	hop_dist = collections.Counter(hop)
-	'''
-	#plt.loglog(hop_dist.values())
-	#plt.show()
-		#print >> f, node, k_hops(G,node,2)
-
 if __name__ == '__main__':
 		main()	
 
